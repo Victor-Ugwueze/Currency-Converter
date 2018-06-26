d7a73878c53e58d9096d8810309c7437f06c77d3
Victor-Ugwueze <victorugwueze@gmail.com>
2018-06-26 02:19:59 +0100

    added app.css

--- docs/index.js
+++ docs/index.js
@@ -1,12 +1,23 @@
-'use strict';
-
-if (!navigator.serviceWorker) {
-  console.log("no");
-} else {
-  navigator.serviceWorker.register('sw/index.js').then(function (registration) {
-    console.log('Service worker registration succeeded:', registration);
-  }).catch(function (error) {
-    console.log('Service worker registration failed:', error);
-  });
-  console.log("yes");
+if(!navigator.serviceWorker){
+   console.log("no");
+}else{
+    navigator.serviceWorker.register('sw/index.js').then((registration)=> {
+        console.log('Service worker registration succeeded:', registration);
+      }).catch((error) =>{
+        console.log('Service worker registration failed:', error);
+      });
 }
+
+const fromCurrency = document.querySelector("#currency-from");
+const toCurrency = document.querySelector("#currency-to");
+
+const weba = fetch("https://free.currencyconverterapi.com/api/v5/currencies")
+.then(response => response.json())
+.then(data => {
+    console.log(data.results);
+     const currency = data.currencyName;
+  for(let key in data.results){
+      fromCurrency.innerHTML += `<option value="${key}">${key}</option>`;
+      toCurrency.innerHTML += `<option value="${key}">${key}</option>`;
+  }
+})
--- docs/sw/index.js
+++ docs/sw/index.js
@@ -1,20 +1,17 @@
-'use strict';
-
 var staticCacheName = 'currency-catch-v1';
 
-self.addEventListener('install', function (event) {
-  //   event.waitUntil(
-  //     caches.open(staticCacheName).then(function(cache) {
-  //       return cache.addAll([
-  //         '/skeleton',
-  //         'js/main.js',
-  //         'css/main.css',
-  //         'imgs/icon.png',
-  //         'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-  //         'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-  //       ]);
-  //     })
-  //   );
+self.addEventListener('install', (event)=> {
+//   event.waitUntil(
+//     caches.open(staticCacheName).then(function(cache) {
+//       return cache.addAll([
+//         'js/main.js',
+//         'css/main.css',
+//         'imgs/icon.png',
+//         'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+//         'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+//       ]);
+//     })
+//   );
 });
 
 // self.addEventListener('activate', function(event) {
